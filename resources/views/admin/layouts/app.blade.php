450b59cbe0dfd0b88c01da7d75c9b4242a17f7c2
DevilShekhar <shekhar@eternalhightech.com>
2026-08-19 16:53:19 +0530

    get property search input added

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -407,6 +407,9 @@
     <script src="{{ asset('assets/js/scripts.js') }}"></script>
     <!-- Custom JS -->
     <script src="{{ asset('assets/js/custom.js') }}"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
     @stack('scripts')
 </body>
