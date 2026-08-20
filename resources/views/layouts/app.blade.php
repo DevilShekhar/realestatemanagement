3680b3bf11c056c057cabed9431041591b147828
DevilShekhar <shekhar@eternalhightech.com>
2026-08-20 17:33:51 +0530

    app layout chnages top bar removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,36 +23,7 @@
     <link  rel="icon"  type="image/png" sizes="16x16"   href="{{ asset('assets/img/favicon.png') }}" >
 </head>
 <body>
-    <div class="boxed_wrapper">
-       <!-- Start Top Bar style2 -->
-        <section class="top-bar-style2">
-            <div class="top-style2 clearfix">
-                <div class="top-style2-left">
-                    <p><span class="icon-music"></span>We have Answers</p>
-                    <ul>
-                        <li>+324 123 45 978</li>
-                        <li>abc@yourdomain.com</li>
-                        <li>Mon - Sat: 9am to 6pm</li>
-                    </ul>
-                </div>
-                <div class="top-style2-right">
-                    <ul class="top-right-menu">
-                        <li><a href="#">Partners</a></li>
-                        <li><a href="#">Media</a></li>
-                        <li><a href="#">360O View</a></li>
-                        <li><a href="#">FAQ</a></li>
-                    </ul>
-                    <ul class="topbar-social-links">
-                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </section>
-        <!-- End Top Bar style2 -->
-
+    <div class="boxed_wrapper">       
         <!--Start Main Header-->
         <header class="main-header header-style2 stricky">
             <div class="inner-container clearfix">
